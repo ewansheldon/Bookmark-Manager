c404ccf23a12ef3b513b659ecaab4f71426ea6f1
ercekal <ercekalabalikoglu@gmail.com>
2016-09-06 12:29:00 +0100

    #12 was done with datacleaner

--- spec/features/see_links_spec.rb
+++ spec/features/see_links_spec.rb
@@ -1,14 +1,22 @@
 require 'spec_helper'
 
 feature 'To see a list of links of the homepage' do
+
+  before :each do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
+  end
+
+  after :each do
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
+  end
   scenario 'a user visits the homepage, they will see a list of links' do
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
-
     visit '/links'
-
     expect(page.status_code).to eq 200
     within 'ul#links' do
-      expect(page).to have_content('Makers Academy')
+    expect(page).to have_content('Makers Academy')
     end
   end
 
@@ -22,4 +30,6 @@ feature 'To see a list of links of the homepage' do
       expect(page).to have_content('Yahoo')
     end
   end
+
+
 end
