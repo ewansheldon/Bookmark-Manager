6bb0c5d62269fffd7ff5260725dd93eccc0522df
ercekal <ercekalabalikoglu@gmail.com>
2016-09-06 11:27:56 +0100

    feature test for link url addition is written and received error

--- spec/features/see_links_spec.rb
+++ spec/features/see_links_spec.rb
@@ -4,11 +4,22 @@ feature 'To see a list of links of the homepage' do
   scenario 'a user visits the homepage, they will see a list of links' do
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
 
-      visit '/links'
+    visit '/links'
 
-      expect(page.status_code).to eq 200
-      within 'ul#links' do
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
       expect(page).to have_content('Makers Academy')
-      end
     end
   end
+
+  scenario 'user can add a new link' do
+    visit '/links/new'
+    fill_in('Bookmark title', with: 'Yahoo')
+    fill_in('Link url', with: 'www.yahoo.com')
+    click_button('Submit')
+    visit '/links'
+    within 'ul#links' do
+      expect(page).to have_content('Yahoo')
+    end
+  end
+end
