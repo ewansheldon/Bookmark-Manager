6f1171cdf547489613cd70dfa658b0a229d442b7
James Stonehill <jamesstonehill@gmail.com>
2016-09-07 15:48:26 +0100

    pass over to E

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -6,12 +6,19 @@ require 'dm-postgres-adapter'
 feature 'tests the user stuff' do
 
   scenario 'homepage signup' do
-    visit '/'
+    visit '/users/new'
     fill_in('email', with: 'something@example.com')
     fill_in('password', with: 'password')
     click_button('submit')
     users = User.all
     expect{users.count}.to change_by{1}
   end
+
+  scenario 'I can sign up as a new user' do
+    expect {sign_up}.to change(User, :count).by(1)
+    expect(page).to have_content('Welcome, alice@example.com')
+    expect(User.first.eamil).to eq('alice@example.com')
+  end
+
 
 end
